e016a7412335d54e551372d26bc02c864e93cd71
Victor Nilsson <victor.nilsson@aalto.fi>
2024-03-06 16:39:52 +0200

    Added the ability to create tabs, developed on the table to include it as a separate tab

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,13 +2,17 @@ import Visuals.Table
 import scalafx.application.JFXApp3
 import scalafx.event.ActionEvent
 import scalafx.scene.Scene
-import scalafx.scene.control.{Menu, MenuBar, MenuItem, SelectionMode, TableColumn, TableView, Button}
-import scalafx.scene.layout.BorderPane
+import scalafx.scene.control.{Button, Menu, MenuBar, MenuItem, ScrollPane, SelectionMode, Slider, SplitPane, Tab, TabPane, TableColumn, TableView, TextField, TreeView}
+import scalafx.scene.layout.{BorderPane, StackPane}
 import scalafx.stage.FileChooser
 import scalafx.stage.FileChooser.*
 import scalafx.Includes.*
 import scalafx.beans.property.{ReadOnlyStringWrapper, StringProperty}
 import scalafx.collections.ObservableBuffer
+import scalafx.geometry.Orientation
+import scalafx.scene.control.cell.TextFieldTableCell
+
+import scala.io.StdIn.readLine
 
 object Main extends JFXApp3:
 
@@ -24,7 +28,7 @@ object Main extends JFXApp3:
     val scene = Scene(parent = rootPane)
     stage.scene = scene
 
-    //Menu bar
+    /**Menu bar*/
 
     val menuBar = new MenuBar
 
@@ -37,7 +41,8 @@ object Main extends JFXApp3:
     val createMenu = new Menu("Create")
     val createGraph = new MenuItem("Graph")
     val createTable = new MenuItem("Table")
-    createMenu.items = List(createGraph,createTable)
+    val createTab = new MenuItem("Tab")
+    createMenu.items = List(createGraph,createTable, createTab)
 
     val controlMenu = new Menu("Control Panel")
     val hideTable = new MenuItem("Hide Table")
@@ -47,6 +52,7 @@ object Main extends JFXApp3:
 
     rootPane.top = menuBar
 
+    /** Menubar actions */
     //importFile.onAction = Data.DataReader().fetchAPI()
     //exportFile.onAction = ???
     openFile.onAction = (e: ActionEvent) => {
@@ -54,19 +60,103 @@ object Main extends JFXApp3:
       val selected = fileChooser.showOpenDialog(stage)
     }
 
-    /** The code that controls the tables are located under this text */
+    /** Tab Pane */
+
+    val testdata = ObservableBuffer(Table("Amount", "1245", "2023"))
+
+    val tab = new TabPane
+    createTab.onAction = (e: ActionEvent) => tab += makeTab()
+    rootPane.center = tab
+
+
+    def makeTab(): Tab = {
+      val tableView = new TableView[Table](testdata)
+      tableView.prefWidth = 240
+      tableView.prefHeight = 300
+      val stockCol = new TableColumn[Table, String] {
+      text = "Stock Symbol"
+      cellValueFactory = _.value.stock
+      prefWidth = 100
+      }
+      val priceCol = new TableColumn[Table, String] {
+      text = "Price"
+      cellValueFactory = _.value.price
+      prefWidth = 50
+      }
+      val dateCol =  new TableColumn[Table, String] {
+      text = "Date"
+      cellValueFactory = _.value.date
+      prefWidth = 75
+      }
+      tableView.columns ++= List(stockCol, priceCol, dateCol)
+      val scroller = new ScrollPane
+      scroller.content = tableView
+
+      val propertyPane = new ScrollPane
+      val left = new SplitPane
+      left.orientation = Orientation.Vertical
+      left.items ++= List(propertyPane, scroller)
+
+      val topBorder = new BorderPane
+      //content = cards
+      val bottomBorder = new BorderPane
+      //content = graphs
+      val slider = new Slider(0, 800, 0)
+      topBorder.top = slider
+      val right = new SplitPane
+      right.orientation = Orientation.Vertical
+      right.items ++= List(topBorder, bottomBorder)
+      right.dividerPositions = 0.7
+
+      val top = new SplitPane
+      top.items ++= List(left, right)
+      top.dividerPositions = 0.3
+
+      val makeTab = new Tab
+      makeTab.text = "Untitled"
+      makeTab.content = top
+      makeTab
+    }
+    /** Tables */
 
     val data = ObservableBuffer(Table(Data.Label().labelName("NetflixTest.json"), Data.Price().closingPrice("NetflixTest.json").head.toString, "2024-01-02"),
     Table(Data.Label().labelName("NetflixTest.json"), Data.Price().closingPrice("NetflixTest.json")(1).toString, "2024-01-03"),
     Table(Data.Label().labelName("NetflixTest.json"), Data.Price().closingPrice("NetflixTest.json")(2).toString, "2024-01-04"),
     Table(Data.Label().labelName("NetflixTest.json"), Data.Price().closingPrice("NetflixTest.json")(3).toString, "2024-01-05"))
 
-    createTable.onAction = (e: ActionEvent) => {
+    createTable.onAction = (e: ActionEvent) => tab += makeTableTab()
 
-    val table = new TableView[Table](data)
-    table.editable = true
-    table.prefWidth = 400
-    table.prefHeight = 400
+    def makeTableTab(): Tab = {
+    val tableView = new TableView[Table](data)
+    val stockCol = new TableColumn[Table, String] {
+    text = "Stock Symbol"
+    cellValueFactory = _.value.stock
+    prefWidth = 100
+    }
+    val priceCol = new TableColumn[Table, String] {
+    text = "Price"
+    cellValueFactory = _.value.price
+    prefWidth = 50
+    }
+    val dateCol =  new TableColumn[Table, String] {
+    text = "Date"
+    cellValueFactory = _.value.date
+    prefWidth = 75
+    }
+    tableView.columns ++= List(stockCol, priceCol, dateCol)
+
+    val makeTab = new Tab
+    makeTab.text = "Table"
+    makeTab.content = tableView
+    makeTab
+    }
+
+    /**createTable.onAction = (e: ActionEvent) => {
+
+    val tableView = new TableView[Table](data)
+    tableView.prefWidth = 200
+    tableView.prefHeight = 200
+    tableView.editable = true
 
     val stockCol = new TableColumn[Table, String] {
     text = "Stock Symbol"
@@ -84,20 +174,15 @@ object Main extends JFXApp3:
     prefWidth = 75
     }
 
-    hideTable.onAction = (e: ActionEvent) => table.visible = false
+    hideTable.onAction = (e: ActionEvent) => tableView.visible = false
 
-    table.columns ++= List(stockCol, priceCol, dateCol)
+    tableView.columns ++= List(stockCol, priceCol, dateCol)
 
-    table.refresh()
+    tableView.refresh()
 
-    table.selectionModel().setSelectionMode(SelectionMode.Multiple)
+    tableView.selectionModel().setSelectionMode(SelectionMode.Multiple)
 
-    rootPane.center = table
-
-
-    }
-
-
+    rootPane.center = tableView */
 
     //Data.DataReader().fetchAPI()
 
---
+++ src/main/scala/Visuals/Chart.scala
@@ -0,0 +1,5 @@
+package Visuals
+
+class Chart {
+
+}
